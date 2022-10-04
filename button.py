a49edfc1b87e92f08cceb2f080ebf1f1e1727d13
aryamasharma <aryama.subbu@gmail.com>
2022-10-04 15:07:16 +0545

    :tada: buttons

--- button.py
+++ button.py
@@ -3,9 +3,11 @@ from tkinter import *
 root = Tk()
 
 def myClick():
-    
+  myLabel= Label(root, text="Look! I clicked a Button")
+  myLabel.pack()
 
-myButton= Button(root,text="Click Me",padx=50,pady=50)
+
+myButton= Button(root,text="Click Me",command=myClick, fg="blue",bg="black")
 myButton.pack()
 root.mainloop()
 
