c71afeb3ec6ffc0068937afb59d44a0b69b0c1d0
aryamasharma <aryama.subbu@gmail.com>
2022-10-10 20:47:15 +0545

    :tada: new window

--- newwindow.py
+++ newwindow.py
@@ -2,13 +2,16 @@ from tkinter import *
 from PIL import ImageTk,Image
 root=Tk()
 root.title("my window")
-btn=Button(root, text="Open second window", command=open()).pack()
+
 
 def open():
  top= Toplevel()
  top.title("new window")
  label = Label(top, text="this is my new window")
+ global my_img
  my_img=ImageTk.PhotoImage(Image.open("images.png"))
  my_label= Label(top,image=my_img).pack()
  label.pack()
+
+btn=Button(root, text="Open second window", command=open).pack()
 root.mainloop()
