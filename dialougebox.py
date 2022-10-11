ae67321fbebeb4a693b648a4255b080d55d0e5da
aryamasharma <aryama.subbu@gmail.com>
2022-10-11 11:37:18 +0545

    :tada: Files dialog box

--- dialougebox.py
+++ dialougebox.py
@@ -6,5 +6,7 @@ root= Tk()
 root.title("learn dialouge box")
 
 root.filename = filedialog.askopenfilename(initialdir="/gui", title="Select a File",filetypes=(("png files","*.png"),("all files","*.*")))
-
+my_label= Label(root, text=root.filename).pack()
+my_image= ImageTk.PhotoImage(Image.open(root.filename))
+my_image_label= Label(image=my_image).pack()
 root.mainloop()
