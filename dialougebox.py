91bf5d05862f112c373e33aff56868a4a1764898
aryamasharma <aryama.subbu@gmail.com>
2022-10-11 11:38:42 +0545

    :tada: Files dialog box

--- dialougebox.py
+++ dialougebox.py
@@ -5,8 +5,12 @@ from tkinter import filedialog
 root= Tk()
 root.title("learn dialouge box")
 
-root.filename = filedialog.askopenfilename(initialdir="/gui", title="Select a File",filetypes=(("png files","*.png"),("all files","*.*")))
-my_label= Label(root, text=root.filename).pack()
-my_image= ImageTk.PhotoImage(Image.open(root.filename))
-my_image_label= Label(image=my_image).pack()
+def open():
+    root.filename = filedialog.askopenfilename(initialdir="/gui", title="Select a File",filetypes=(("png files","*.png"),("all files","*.*")))
+    my_label= Label(root, text=root.filename).pack()
+    global my_image
+    my_image= ImageTk.PhotoImage(Image.open(root.filename))
+    my_image_label= Label(image=my_image).pack()
+
+my_button = Button(root, text="Open file", command= open)
 root.mainloop()
